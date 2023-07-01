e32c40250e2c23caff8e63997631d8a64e18c6f7
Praisg <gavipraise@gmail.com>
2023-07-01 15:01:05 -0700

    sorts

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sort the list in insertion manner
+ * @list: UnSorted_Linked_list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node;
+
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swap_node(node, list);
+			print_list(*list);
+		}
+		node = node->next;
+	}
+}
+/**
+ *swap_node - help swap nodee
+ *@node: node
+ *@list: list ptr to node
+ *Return: 0 always for success
+ */
+
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
+}
